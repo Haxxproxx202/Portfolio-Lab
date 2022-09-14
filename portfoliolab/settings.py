6c78db7c80e4aab553ec5e0cd9e815569ba60598
Haxxproxx202 <pjanecki88@gmail.com>
2022-09-14 19:38:58 +0200

    deployment - add staticfiles2

--- portfoliolab/settings.py
+++ portfoliolab/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-6vb2)#!(#552n*gp)%+!#tubrot6w7w(9mc0qtw!36d)1hpbie'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
